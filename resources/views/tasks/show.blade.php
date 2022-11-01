31c81782ddeacad2145e56a702f679d2810b935c
github <github>
2022-11-01 18:59:22 +0900

    新規作成

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -2,6 +2,23 @@
 
 @section('content')
 
-<!-- ここにページ毎のコンテンツを書く -->
+
+<h1>id = {{ $task->id }} のタスク詳細ページ</h1>
+
+<table class="table table-bordered">
+    <tr>
+        <th>id</th>
+        <td>{{ $task->id }}</td>
+    </tr>
+    <tr>
+        <th>タスク</th>
+        <td>{{ $task->content }}</td>
+    </tr>
+    <tr>
+        <th>いつまでに</th>
+        <td>{{ $task->limit }}</td>
+    </tr>
+</table>
+
 
 @endsection
