f467bcddd3030e85eb1c1707c0634e3d5ba860b1
github <github>
2022-11-01 18:59:06 +0900

    新規作成

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,6 +2,27 @@
 
 @section('content')
 
-<!-- ここにページ毎のコンテンツを書く -->
+    <h1>新規タスク入力</h1>
+
+    <div class="row">
+        <div class="col-6">
+            {!!Form::model($task, ['route'=> 'tasks.store']) !!}
+
+                <div class="form-group">
+                    {!! Form::label('content','タスク:')!!}
+                    {!! Form::text('content', null,['class' => 'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('limit','いつまで:')!!}
+                    {!! Form::text('limit', null,['class' => 'form-control']) !!}
+                </div>
+
+                {!! Form::submit('登録',['class' => 'btn btn-primary']) !!}
+
+            {!! Form::close() !!}
+            
+        </div>
+    </div>
 
 @endsection
