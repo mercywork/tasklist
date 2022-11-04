cb271d7f18370a8b0816fc6913f3186d8791b0ec
github <github>
2022-11-04 21:03:31 +0900

    タスク編集ページ作成

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -19,6 +19,17 @@
         <td>{{ $task->limit }}</td>
     </tr>
 </table>
+    <div class="edit">
 
+    {!! link_to_route('tasks.edit','内容を変更する', ['task' => $task->id], ['class' => 'btn btn-primary']) !!}
+
+    </div>
+
+     {{-- メッセージ削除フォーム --}}
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+    
+    {!! Form::close() !!}
 
 @endsection
